3024b8d1ec8c1e861d2fc44d2d81a89b68cd4122
JasonNguyen067 <jasonnguyen7506@gmail.com>
2026-02-11 21:20:40 -0800

    route updates

---
+++ app/(main)/profile/edit/page.js
@@ -0,0 +1,3 @@
+export default function EditAccountPage() {
+  return <div>Edit Account</div>;
+}
---
+++ app/(main)/profile/privacy-and-terms/page.js
@@ -0,0 +1,3 @@
+export default function PrivacyAndTermsPage() {
+  return <div>Privacy and Terms</div>;
+}
