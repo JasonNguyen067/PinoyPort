35e13681b6e84b841a10c9545e84ef34e0caedf2
Harrison Tran <harrisontran357@gmail.com>
2026-02-12 20:51:13 -0800

    responsive dimensions, priority on image, removed navbar from loading

--- app/loading.js
+++ app/loading.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import logo from "../public/logo.png"
-import loadingbar from "../public/loadingbar.png"
+import React from "react";
+import logo from "../public/logo.png";
+import loadingbar from "../public/loadingbar.png";
 import Image from "next/image";
 
 export default function Loading() {
   return (
-    <div className="flex bg-white w-screen h-screen justify-center">
-      <Image 
+    <div className="fixed inset-0 z-[9999] flex bg-white flex-col items-center justify-center overflow-hidden">
+      {/* responsive size, priority so it loads first */}
+      <Image
         src={logo}
-        className='relative top-[33vw] w-[34vw] h-[50vw]'
         alt="Loading Logo"
+        priority
+        sizes="(max-width: 640px) 180px, (max-width: 1024px) 240px, 320px"
+        className="h-auto w-[45vw] max-w-[180px] object-contain sm:w-[40vw] sm:max-w-[220px] md:max-w-[260px] lg:max-w-[320px]"
       />
       {/* TODO: make loading bar dynamic */}
-      <Image 
+      <Image
         src={loadingbar}
-        className='absolute bottom-0 left-0 h-[36px]'
-        alt='Loading Bar'
+        alt="Loading Bar"
+        className="absolute bottom-0 left-0 h-8 w-full object-cover object-left sm:h-9 md:h-10 lg:h-12"
       />
     </div>
   );
