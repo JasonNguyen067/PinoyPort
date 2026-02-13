7fcb754736882c4b89ba0dace4e0dd5d23079a71
Harrison Tran <harrisontran357@gmail.com>
2026-02-12 20:52:19 -0800

    added promise timeout for testing purposes

--- app/(main)/community/page.js
+++ app/(main)/community/page.js
@@ -1,3 +1,6 @@
-export default function CommunityFeedPage() {
+export default async function CommunityFeedPage() {
+  // TODO: remove this after testing
+  await new Promise(r => setTimeout(r, 3000));
+  
   return <div>Community Feed</div>;
 }
