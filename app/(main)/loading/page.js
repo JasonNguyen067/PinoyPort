7c145e99f0ba7002bd66d0703fb47135e1dc3bab
JasonNguyen067 <jasonnguyen7506@gmail.com>
2026-03-02 20:02:24 -0800

    fix for deployment

--- app/(main)/loading/page.js
+++ app/(main)/loading/page.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import logo from "../../../public/logo.png"
-import loadingbar from "../../../public/loadingbar.png"
 import Image from "next/image";
 
 export default function Loading() {
   return (
     <div className="flex bg-white w-screen h-screen justify-center">
-      <Image 
-        src={logo}
+      <Image
+        src="/logo.png"
+        width={500}
+        height={500}
         className='relative top-[33vw] w-[34vw] h-[50vw]'
         alt="Loading Logo"
       />
       {/* TODO: make loading bar dynamic */}
-      <Image 
-        src={loadingbar}
+      <Image
+        src="/loadingbar.png"
+        width={1920}
+        height={36}
         className='absolute bottom-0 left-0 h-[36px]'
         alt='Loading Bar'
       />
